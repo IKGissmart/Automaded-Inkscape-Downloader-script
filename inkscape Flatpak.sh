01922272343afc34c27cbbeb41494aae3fe47645
IKGissmart <84079013+IKGissmart@users.noreply.github.com>
2021-09-15 13:12:49 +0500

    Update inkscape Flatpak.sh

--- inkscape Flatpak.sh
+++ inkscape Flatpak.sh
@@ -4,6 +4,10 @@ if [ $? -ne 0 ]; then
   echo "Error: Please install Flatpak first (https://flatpak.org/setup)." >&2
   exit 1
 fi
+cd ~/Downloads 
+mkdir Flathub-Inkscape
+cd Flathub-Inkscape
+wget https://dl.flathub.org/repo/appstream/org.inkscape.Inkscape.flatpakref
 flatpak install flathub org.inkscape.Inkscape -y 
 echo "Done"
 exit 
