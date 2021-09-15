cfc819ef0ace5ae392b4c057c3e188e79c7877c9
IKGissmart <84079013+IKGissmart@users.noreply.github.com>
2021-09-15 14:29:11 +0500

    Add files via upload

---
+++ inkscape downloader.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+cd ~/Downloads 
+mkdir inkscape-appimage
+cd inkscape-appimage 
+wget https://inkscape.org/gallery/item/26933/Inkscape-c4e8f9e-x86_64.AppImage -O Inkscape_1.1_Appimageversion
+echo "Done"
+exit 
